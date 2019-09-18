f5aa9f3ca0dc2a272277a2424932f1e8c4fe4c2e
lhz <1541860665@qq.com>
2019-09-18 15:11:35 +0800

    test

--- src/main/java/com/example/demo/Helloworld.java
+++ src/main/java/com/example/demo/Helloworld.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Helloworld {
-    @RequestMapping("/helloworld")
+    @RequestMapping("/test")
     public String HelloWorld(){
         return "Hello World!";
     }
