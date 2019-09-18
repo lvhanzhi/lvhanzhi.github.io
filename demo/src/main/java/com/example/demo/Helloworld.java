f4437ac9e4b1487735c1d872e6cbdeb78b46d449
lhz <1541860665@qq.com>
2019-09-18 09:52:24 +0800

    demo

---
+++ demo/src/main/java/com/example/demo/Helloworld.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworld {
+    @RequestMapping("/helloworld")
+    public String HelloWorld(){
+        return "Hello World!";
+    }
+}
